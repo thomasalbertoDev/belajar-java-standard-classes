3e57e3ef2258698012b00ab0310fd225c7c9a191
thomasalbertoDev <thomasalberto456@gmail.com>
2024-09-05 16:17:47 +0700

    learn: belajar math class

---
+++ src/rumah/kodingku/classess/MathApp.java
@@ -0,0 +1,15 @@
+package rumah.kodingku.classess;
+
+public class MathApp {
+
+    public static void main(String[] args) {
+        // Math Class
+
+        var min = Math.min(1000, 2000); // Mengecek nilai terkecil dari 1000 dan 2000
+        var max = Math.max(1000, 2000); // Mengecek nilai terbesar dari 1000 dan 2000
+
+        System.out.println(min);
+        System.out.println(max);
+
+    }
+}
